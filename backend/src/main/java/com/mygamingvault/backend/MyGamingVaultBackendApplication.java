fc64281b36eaf0da2d965ea9d643a99bcb5887e1
RubenAlonso <rac00030@red.ujaen.es>
2025-07-02 18:29:42 +0200

    [BACKEND] init files

---
+++ backend/src/main/java/com/mygamingvault/backend/MyGamingVaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mygamingvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGamingVaultBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGamingVaultBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/mygamingvault/backend/MyGamingVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygamingvault.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGamingVaultBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
