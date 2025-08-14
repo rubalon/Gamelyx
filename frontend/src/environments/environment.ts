625e8f4c00c9f68df8d14bf2e90e0d85d2d7504a
RubenAlonso <rac00030@red.ujaen.es>
2025-08-14 21:59:10 +0200

    [FRONTEND] environment normal con prod mode

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  apiUrl: 'https://api.gamelyx.es/api', // URL de producción
   version: '1.0.0',
   features: {
-    enableGoogleAuth: false, 
+    enableGoogleAuth: false,
     enableEmailVerification: true
   }
 };
